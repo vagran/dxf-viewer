646421b85cafaaff02639c7b738932b6b4ac1258
Artyom Lebedev <vagran.ast@gmail.com>
2023-04-04 12:18:14 +0300

    Fix indents size.

--- src/hatch/patternFillCalculator.js
+++ src/hatch/patternFillCalculator.js
@@ -4,140 +4,139 @@ import { Matrix2 } from '../math/Matrix2'
 const EPSILON = 1e-6
 
 export class HatchCalculator {
-  boundaryPaths
+    boundaryPaths
 
-  /**
-   * Arrays of `Path` to use as boundary, and each `Path` is array of `Point`.
-   * 
-   * @param {Vector2[]} boundaryPaths 
-   */
-  constructor(boundaryPaths) {
-    this.boundaryPaths = boundaryPaths
-
-  }
-
-  /**
-   * Clip `line` on masking defined by `boundaryPaths`
-   * 
-   * @param {[Vector2, Vector2]} line 
-   * @returns {[Vector2, Vector2][]} clipped line segments
-   */
-  ClipLine(line) {
-    // concat
-    const intersections = this.boundaryPaths.reduce((intersections, path) => {
-      intersections.push(...this._GetIntersections(line, path))
-      return intersections
-    }, []).sort((a, b) => a - b)
-
-    const isFirstInside = this._IsInside(line[0])
-    const segments = isFirstInside
-      ? [0, ...intersections]
-      : intersections
-
-    return Array.from(new Array(segments.length / 2), (_, i) => {
-      const diff = line[1].clone().sub(line[0])
-      const start = diff.copy().multiplyScalar(segments[2 * i]).add(line[0])
-      const end = diff.copy().multiplyScalar(segments[2 * i + 1]).add(line[0])
-      return [start, end]
-    })
-  }
-
-  /**
-   * Compute intersection of `line` and `path` and return
-   * each interpolation constant `t0`s. Note that they're not sorted.
-   * Each `t0` is in [0, 1).
-   * 
-   * @param {[Vector2, Vector2]} line 
-   * @param {Vector2[]} path 
-   * @returns {number[]} arrays of intersection lerp param t0 for line
-   */
-  _GetIntersections(line, path) {
-    let count = 0
-    const result = new Array(path.length)
-    for (let i = 0; i < path.length; ++i) {
-      const j = (i + 1) % path.length
-      const t0 = this._GetIntersection(line, path[i], path[j])
-
-      if (t0 === undefined) continue
-      result[count++] = t0
+    /**
+     * Arrays of `Path` to use as boundary, and each `Path` is array of `Point`.
+     *
+     * @param {Vector2[]} boundaryPaths
+     */
+    constructor(boundaryPaths) {
+        this.boundaryPaths = boundaryPaths
     }
-    result.length = count
-    return result
-  }
 
-  /**
-   * Compute intersection of two lines and return `t0` such that
-   * line0[0] + t0 * (line0[1] - line0[0]) = intersection of `line1`.
-   * 
-   * Note that start point of line is inclusive while the other is exclusive.
-   * If there is no such `t0`, returns `undefined`
-   * 
-   * @param {[Vector2, Vector2]} line0 
-   * @param {[Vector2, Vector2]} line1 
-   * @returns {number | undefined} t0
-   */
-  _GetIntersection(line0, line1) {
-    const [s0, e0] = line0
-    const [s1, e1] = line1
-    const diff0 = e0.clone().sub(s0)
-    const diff1 = s1.clone().sub(e1)
-    
-    const A = (new Matrix2(
-      diff0.x, diff1.x,
-      diff0.y, diff1.y,
-    ))
-    const det = A.det()
-    if (Math.abs(det) < EPSILON) return undefined
+    /**
+     * Clip `line` on masking defined by `boundaryPaths`
+     *
+     * @param {[Vector2, Vector2]} line
+     * @returns {[Vector2, Vector2][]} clipped line segments
+     */
+    ClipLine(line) {
+        // concat
+        const intersections = this.boundaryPaths.reduce((intersections, path) => {
+            intersections.push(...this._GetIntersections(line, path))
+            return intersections
+        }, []).sort((a, b) => a - b)
 
-    const AInverse = A.inverse(det)
-    const b = s1.clone().sub(s0)
-    const { x: t0, y: t1 } = AInverse.multiply(b)
+        const isFirstInside = this._IsInside(line[0])
+        const segments = isFirstInside
+            ? [0, ...intersections]
+            : intersections
 
-    // one side is exclusive to avoid duplicated counting, 
-    // when intersection point is exactly a vertex of edges
-    if (t0 < 0 || t0 >= 1 || t1 < 0 || t1 >= 1) return undefined
-    return t0
-  }
-
-  /**
-   * Return whether `point` is inside of union of `paths`
-   * 
-   * @param {Vector2} point 
-   * @returns {boolean} is `point` inside of union of `paths`
-   */
-  _IsInside(point) {
-    const p = this._GetFarthestCenterOnPath(point)
-    const q = p.clone().multiplyScalar(2).sub(point)
-
-    // use odd even rule
-    let count = 0
-    for (const path of this.paths) {
-      count += this._GetIntersections([point, q], path).length
+        return Array.from(new Array(segments.length / 2), (_, i) => {
+            const diff = line[1].clone().sub(line[0])
+            const start = diff.copy().multiplyScalar(segments[2 * i]).add(line[0])
+            const end = diff.copy().multiplyScalar(segments[2 * i + 1]).add(line[0])
+            return [start, end]
+        })
     }
-    return count % 2 === 1
-  }
 
-  /**
-   * Return the most farthest edge's center point of paths
-   * 
-   * @param {Vector2} point 
-   * @returns {Vector2} Farthest center of edges on path
-   */
-  _GetFarthestCenterOnPath(point) {
-    let dMaxPoint = null
-    let dMaxSq = 0
-    for (const path of this.boundaryPaths) {
-      for (let i = 0; i < path.length; ++i) {
-        const j = (i + 1) % path.length
-        const m = path[i].clone().add(path[j]).divideScalar(2)
-        const dSq = point.distanceToSquared(m)
+    /**
+     * Compute intersection of `line` and `path` and return
+     * each interpolation constant `t0`s. Note that they're not sorted.
+     * Each `t0` is in [0, 1).
+     *
+     * @param {[Vector2, Vector2]} line
+     * @param {Vector2[]} path
+     * @returns {number[]} arrays of intersection lerp param t0 for line
+     */
+    _GetIntersections(line, path) {
+        let count = 0
+        const result = new Array(path.length)
+        for (let i = 0; i < path.length; ++i) {
+            const j = (i + 1) % path.length
+            const t0 = this._GetIntersection(line, path[i], path[j])
 
-        if (dMaxSq < dSq) {
-          dMaxSq = dSq
-          dMaxPoint = m
+            if (t0 === undefined) continue
+            result[count++] = t0
         }
-      }
+        result.length = count
+        return result
     }
-    return dMaxPoint
-  }
+
+    /**
+     * Compute intersection of two lines and return `t0` such that
+     * line0[0] + t0 * (line0[1] - line0[0]) = intersection of `line1`.
+     *
+     * Note that start point of line is inclusive while the other is exclusive.
+     * If there is no such `t0`, returns `undefined`
+     *
+     * @param {[Vector2, Vector2]} line0
+     * @param {[Vector2, Vector2]} line1
+     * @returns {number | undefined} t0
+     */
+    _GetIntersection(line0, line1) {
+        const [s0, e0] = line0
+        const [s1, e1] = line1
+        const diff0 = e0.clone().sub(s0)
+        const diff1 = s1.clone().sub(e1)
+
+        const A = (new Matrix2(
+            diff0.x, diff1.x,
+            diff0.y, diff1.y,
+        ))
+        const det = A.det()
+        if (Math.abs(det) < EPSILON) return undefined
+
+        const AInverse = A.inverse(det)
+        const b = s1.clone().sub(s0)
+        const { x: t0, y: t1 } = AInverse.multiply(b)
+
+        // one side is exclusive to avoid duplicated counting,
+        // when intersection point is exactly a vertex of edges
+        if (t0 < 0 || t0 >= 1 || t1 < 0 || t1 >= 1) return undefined
+        return t0
+    }
+
+    /**
+     * Return whether `point` is inside of union of `paths`
+     *
+     * @param {Vector2} point
+     * @returns {boolean} is `point` inside of union of `paths`
+     */
+    _IsInside(point) {
+        const p = this._GetFarthestCenterOnPath(point)
+        const q = p.clone().multiplyScalar(2).sub(point)
+
+        // use odd even rule
+        let count = 0
+        for (const path of this.paths) {
+            count += this._GetIntersections([point, q], path).length
+        }
+        return count % 2 === 1
+    }
+
+    /**
+     * Return the most farthest edge's center point of paths
+     *
+     * @param {Vector2} point
+     * @returns {Vector2} Farthest center of edges on path
+     */
+    _GetFarthestCenterOnPath(point) {
+        let dMaxPoint = null
+        let dMaxSq = 0
+        for (const path of this.boundaryPaths) {
+            for (let i = 0; i < path.length; ++i) {
+                const j = (i + 1) % path.length
+                const m = path[i].clone().add(path[j]).divideScalar(2)
+                const dSq = point.distanceToSquared(m)
+
+                if (dMaxSq < dSq) {
+                    dMaxSq = dSq
+                    dMaxPoint = m
+                }
+            }
+        }
+        return dMaxPoint
+    }
 }
--- src/math/Matrix2.js
+++ src/math/Matrix2.js
@@ -1,52 +1,52 @@
 import { Vector2 } from "three"
 
 export class Matrix2 {
-  /**
-   * 
-   * @param {number} a00 
-   * @param {number} a01 
-   * @param {number} a10 
-   * @param {number} a11 
-   */
-  constructor(a00, a01, a10, a11) {
-    this.a00 = a00
-    this.a01 = a01
-    this.a10 = a10
-    this.a11 = a11
-  }
+    /**
+     *
+     * @param {number} a00
+     * @param {number} a01
+     * @param {number} a10
+     * @param {number} a11
+     */
+    constructor(a00, a01, a10, a11) {
+        this.a00 = a00
+        this.a01 = a01
+        this.a10 = a10
+        this.a11 = a11
+    }
 
-  /**
-   * Multiply Vector2 to this matrix as Av
-   * @param {Vector2} v 
-   * @returns {Vector2} transformed v
-   */
-  multiply(v) {
-    return new Vector2(
-      this.a00 * v.x + this.a01 * v.y,
-      this.a10 * v.x + this.a11 * v.y,
-    )
-  }
+    /**
+     * Multiply Vector2 to this matrix as Av
+     * @param {Vector2} v
+     * @returns {Vector2} transformed v
+     */
+    multiply(v) {
+        return new Vector2(
+            this.a00 * v.x + this.a01 * v.y,
+            this.a10 * v.x + this.a11 * v.y,
+        )
+    }
 
-  /**
-   * Return determinant of this matrix
-   * @returns {number}
-   */
-  det() {
-    return this.a00 * this.a11 - this.a01 * this.a10
-  }
+    /**
+     * Return determinant of this matrix
+     * @returns {number}
+     */
+    det() {
+        return this.a00 * this.a11 - this.a01 * this.a10
+    }
 
-  /**
-   * Return inverse of this matrix. If inverse is not exists i.e.
-   * this.det() === 0, return `undefined`.
-   * @param {number | undefined} - in case of pre computed determinant, you may pass it to parameter
-   * @returns {Matrix2 | undefined} inverse of this matrix
-   */
-  inverse(determinant = det()) {
-    if (determinant === 0) return undefined
+    /**
+     * Return inverse of this matrix. If inverse is not exists i.e.
+     * this.det() === 0, return `undefined`.
+     * @param {number | undefined} - in case of pre computed determinant, you may pass it to parameter
+     * @returns {Matrix2 | undefined} inverse of this matrix
+     */
+    inverse(determinant = det()) {
+        if (determinant === 0) return undefined
 
-    return new Matrix2(
-      this.a11 / determinant, -this.a01 / determinant,
-      -this.a10 / determinant, this.a00 / determinant,
-    )
-  }
+        return new Matrix2(
+            this.a11 / determinant, -this.a01 / determinant,
+            -this.a10 / determinant, this.a00 / determinant,
+        )
+    }
 }
