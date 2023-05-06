74f3f56a1b4c54dd273c16018bfa39d4f7190b48
Willow <605728901@qq.com>
2023-05-06 18:11:56 +0800

    fix embeddedObject show problem

--- src/parser/entities/mtext.js
+++ src/parser/entities/mtext.js
@@ -5,48 +5,79 @@ export default function EntityParser() {}
 
 EntityParser.ForEntityName = 'MTEXT';
 
-EntityParser.prototype.parseEntity = function(scanner, curr) {
-    var entity = { type: curr.value };
+EntityParser.prototype.parseEntity = function (scanner, curr) {
+    var entity = {type: curr.value, embeddedObject: false};
     curr = scanner.next();
-    while(curr !== 'EOF') {
-        if(curr.code === 0) break;
+    while (curr !== 'EOF') {
+        if (curr.code === 0) break;
 
-        switch(curr.code) {
-        case 3:
-        case 1:
-            entity.text ? entity.text += curr.value : entity.text = curr.value;
-            break;
-        case 10:
-            entity.position = helpers.parsePoint(scanner);
-            break;
-        case 11:
-            entity.direction = helpers.parsePoint(scanner);
-            break;
-        case 40:
-            //Note: this is the text height
-            entity.height = curr.value;
-            break;
-        case 41:
-            entity.width = curr.value;
-            break;
-        case 44:
-            entity.lineSpacing = curr.value;
-            break;
-        case 50:
-            entity.rotation = curr.value;
-            break;
-        case 71:
-            entity.attachmentPoint = curr.value;
-            break;
-        case 72:
-            entity.drawingDirection = curr.value;
-            break;
-        case 101:
-            helpers.skipEmbeddedObject(scanner);
-            break;
-        default:
-            helpers.checkCommonEntityProperties(entity, curr, scanner);
-            break;
+        switch (curr.code) {
+            case 3:
+            case 1:
+                entity.text ? entity.text += curr.value : entity.text = curr.value;
+                break;
+            case 10:
+                if(!entity.embeddedObject){
+                    entity.position = helpers.parsePoint(scanner);
+                }
+                break;
+            case 11:
+                if(!entity.embeddedObject){
+                    entity.direction = helpers.parsePoint(scanner);
+                }
+                break;
+            case 40:
+                //Note: this is the text height
+                if(!entity.embeddedObject){
+                    entity.height = curr.value;
+                }
+                break;
+            case 41:
+                if(!entity.embeddedObject){
+                    entity.width = curr.value;
+                }
+                break;
+            case 44:
+                if(!entity.embeddedObject){
+                    entity.lineSpacing = curr.value;
+                }else{
+                    // this is the width of each column
+                    entity.embeddedObjectWidth = curr.value;
+                }
+                break;
+            case 45:
+                if(entity.embeddedObject){
+                    entity.embeddedObjectGutterWidth = curr.value;
+                }
+                break;
+            case 46:
+                if(!entity.embeddedObject){
+                    entity.boxHeight = curr.value;
+                }
+                break;
+            case 50:
+                entity.rotation = curr.value;
+                break;
+            case 71:
+                if(!entity.embeddedObject){
+                    entity.attachmentPoint = curr.value;
+                }
+                break;
+            case 72:
+                if(!entity.embeddedObject){
+                    entity.drawingDirection = curr.value;
+                }else{
+                    // this is numbers of columns
+                    entity.embeddedObjectColumn = curr.value;
+                }
+                break;
+            case 101:
+                entity.embeddedObject = true;
+                // helpers.skipEmbeddedObject(scanner);
+                break;
+            default:
+                helpers.checkCommonEntityProperties(entity, curr, scanner);
+                break;
         }
         curr = scanner.next();
     }
