7d0fe43bb25c1eb13688b903bf720c1f81197bf4
Artyom Lebedev <vagran.ast@gmail.com>
2023-06-02 13:38:47 +0300

    Sketching parser concept.

--- src/parser/DxfScheme.ts
+++ src/parser/DxfScheme.ts
@@ -1,40 +1,111 @@
-import { OrderedGroup, UnorderedGroup, OneOfGroup } from "./Scheme"
+import * as Scheme from "./Scheme"
+import {OrderedGroup} from "./Scheme"
+import Token, {TokenValue} from "./Token"
 
-const scheme = {
-    type: OrderedGroup,
-    content: [{
-        /* Sections in any order. */
-        type: UnorderedGroup, //XXX requireAll
-        content: [{
-            type: Section("HEADER"),
-            content: {
-                type: OrderedGroup,
-                q: "*",
-                id: "var",
-                content: [{
-                    type: Tag(9),
-                    id: "name"
-                },
-                {
-                    type: OneOfGroup,
-                    id: "value",
-                    content: [{
-                        type: Vector(10)
-                    }, {
-                        type: AnyTag
-                    }]
-                }]
-            }
-        }, {
-            type: Section("BLOCKS")
 
-        }],
-        fallback: {
-            type: AnySection
-        }
-    }, {
-        type: Entity("EOF")
-    }]
+type EntityNodeParams = {
+    name: string
 }
+
+/** Matches entity group (code 0) with the specified name. */
+const Entity = Scheme.MakeNodeFactory(class Entity extends Scheme.SchemeTerminalNode<EntityNodeParams> {
+    private readonly name: string
+
+    constructor(parent: Scheme.SchemeNode | null, nodeDesc: Scheme.NodeParams<EntityNodeParams>) {
+        super(parent, nodeDesc)
+        this.name = nodeDesc.name
+    }
+
+    override Match(token: Token): Scheme.MatchResult {
+        if (token.code === 0 && token.value === this.name) {
+            return Scheme.MatchResult.MATCH
+        }
+        return Scheme.MatchResult.NO_MATCH
+    }
+})
+
+type TagNodeParams = {
+    code: number
+    valueTransformer?: Scheme.ValueTransformer
+}
+
+/** Matches tag with the specified code. */
+const Tag = Scheme.MakeNodeFactory(class Tag extends Scheme.SchemeTerminalNode<TagNodeParams> {
+    private readonly code: number
+    private readonly valueTransformer: Scheme.ValueTransformer | null
+
+    constructor(parent: Scheme.SchemeNode | null, nodeDesc: Scheme.NodeParams<TagNodeParams>) {
+        super(parent, nodeDesc)
+        this.code = nodeDesc.code
+        this.valueTransformer = nodeDesc.valueTransformer ?? null
+    }
+
+    override Match(token: Token): Scheme.MatchResult {
+        if (token.code === this.code) {
+            return Scheme.MatchResult.MATCH
+        }
+        return Scheme.MatchResult.NO_MATCH
+    }
+
+    override TransformValue(value: TokenValue): any {
+        if (this.valueTransformer) {
+            return this.valueTransformer(value)
+        }
+        return value
+    }
+})
+
+type SectionNodeParams = {
+    name: string
+    content: Scheme.NodeDesc
+}
+
+/** Matches tag with the specified code. */
+function Section(params: Scheme.NodeParams<SectionNodeParams>): Scheme.NodeDesc {
+    return OrderedGroup({
+        ...params,
+        content: [
+            Entity({name: "SECTION"}),
+            Tag({code: 2, id: "name"}),
+            params.content,
+            Entity({name: "ENDSEC"})
+        ]
+    })
+}
+
+const scheme: Scheme.NodeDesc = OrderedGroup({
+    content: [
+        UnorderedGroup({
+            /* Sections in any order. */
+            //XXX requireAll
+            content: [
+                Section({
+                    name: "Header",
+                    content: OrderedGroup({
+                            q: "*",
+                            id: "var",
+                            content: [
+                                Tag({
+                                    code: 9,
+                                    id: "name"
+                                }),
+                                OneOfGroup({
+                                    id: "value",
+                                    content: [
+                                        Vector(10),
+                                        AnyTag
+                                    ]
+                                })
+                            ]
+                        })
+                    }),
+                Section({name: "BLOCKS"})
+            ],
+            fallback: AnySection
+        }),
+
+        Entity({name: "EOF"})
+    ]
+})
 
 export default scheme
--- src/parser/Scheme.ts
+++ src/parser/Scheme.ts
@@ -1,32 +1,59 @@
-import Token, { TokenValue } from "./Token"
+
+/* ************************************************************************************************/
+/* Scheme declaration stuff. */
+
+export type NodeBaseParams = {
+    /** Node quantifier. */
+    q?: Quantifier | string | Array<number>
+    /** Corresponding AST node is created if ID is specified. */
+    id?: string,
+    /** Grammar symbol ID to refer by {@link NodeRef}. */
+    symbolId?: any,
+    /** First candidate node which matches current token, discards all the rest candidates if any,
+     * when this parameter is set.
+     */
+    exclusiveMatch?: boolean
+}
+
+export type NodeParams<TParams> = NodeBaseParams & TParams
+
+export type NodeFactory<TParams> = (parent: SchemeNode | null, nodeDesc: NodeParams<TParams>) =>
+    SchemeNode
+
+export type NodeDesc = {
+    factory: NodeFactory<any>
+
+    [key: string]: any
+} & NodeBaseParams
+
+export type NodeDescFactory<TParams> = (params: NodeParams<TParams>) => NodeDesc
 
 //XXX
-export function OrderedGroup(...items: any): any {
-    return {}
+export type NodeFactoryParams<T> = T extends NodeFactory<infer TParams> ? TParams : never
+
+
+export interface SchemeNodeImpl<TParams> {
+    new (parent: SchemeNode | null, nodeDesc: NodeParams<TParams>): SchemeNode
 }
 
-export function UnorderedGroup(...items: any): any {
-    return {}
+export function MakeNodeFactory<TParams>(cls: SchemeNodeImpl<TParams>): NodeDescFactory<TParams> {
+    return (params: NodeParams<TParams>) => {
+        return {
+            factory(parent: SchemeNode | null, nodeDesc: NodeParams<TParams>): SchemeNode {
+                return new cls(parent, nodeDesc)
+            },
+            ...params
+        }
+    }
 }
 
-export function OneOfGroup(...items: any): any {
-}
+//XXX
+// export type ValueTransformer = (value: TokenValue) => any
 
-interface SchemeNodeContext {
-    schemeNode: SchemeNode
-    parent: SchemeNodeContext | null
-}
+/* ************************************************************************************************/
+/* Scheme implementation. */
 
-export enum MatchResult {
-    /** Node does not match. */
-    NO_MATCH,
-    /** Node matches. May be concurrent with other match candidates. */
-    MATCH,
-    /** Node matches, and all the other match candidates if any should be discarded. */
-    EXCLUSIVE_MATCH
-}
-
-class Quantifier {
+export class Quantifier {
     constructor(public readonly min: number, public readonly max: number) {}
 
     static FromChar(c: string): Quantifier {
@@ -40,53 +67,197 @@ class Quantifier {
         }
         throw new Error(`Bad quantifier character: ${c}`)
     }
+
+    static SINGLE = new Quantifier(1, 1)
 }
 
-interface SchemeNode {
+class Grammar {
+    readonly symbolsRegistry = new Map<any, SchemeNode>()
+
+    RegisterSymbol(id: any, node: SchemeNode): void {
+        if (this.symbolsRegistry.has(id)) {
+            throw new Error(`Duplicate grammar symbol ID: ${id}`)
+        }
+        this.symbolsRegistry.set(id, node)
+    }
+
+    LookupSymbol(id: any): SchemeNode {
+        const node = this.symbolsRegistry.get(id)
+        if (!node) {
+            throw new Error(`Grammar symbol not found: ${id}`)
+        }
+        return node
+    }
+}
+
+export abstract class SchemeNode {
     /** Leaf node is matched against next token. Interim node is queried for children by calling
      * its `GetNextChild()` method.
      */
-    readonly isLeaf: boolean
+    abstract readonly isTerminal: boolean
 
     readonly quantifier: Quantifier
+
+    readonly id: string | null
+
+    /** Defined in root node only. */
+    readonly grammar?: Grammar
+
+    constructor(readonly parent: SchemeNode | null, nodeDesc: NodeBaseParams) {
+        this.id = nodeDesc.id ?? null
+        if (nodeDesc.q) {
+            if (nodeDesc.q instanceof Quantifier) {
+                this.quantifier = nodeDesc.q
+            } else if (Array.isArray(nodeDesc.q)) {
+                this.quantifier = new Quantifier(nodeDesc.q[0], nodeDesc.q[1])
+            } else {
+                this.quantifier = Quantifier.FromChar(nodeDesc.q)
+            }
+        } else {
+            this.quantifier = Quantifier.SINGLE
+        }
+        if (!parent) {
+            this.grammar = new Grammar()
+        }
+        if (nodeDesc.symbolId !== undefined) {
+            this.GetGrammar().RegisterSymbol(nodeDesc.symbolId, this)
+        }
+    }
+
+    GetRoot(): SchemeNode {
+        let node: SchemeNode = this
+        while (node.parent) {
+            node = node.parent
+        }
+        return node
+    }
+
+    GetGrammar(): Grammar {
+        return this.GetRoot().grammar!
+    }
 }
 
-abstract class SchemeLeafNode implements SchemeNode {
-    readonly isLeaf: boolean = true
+export abstract class SchemeTerminalNode<TParams, TToken> extends SchemeNode {
+    readonly isTerminal: boolean = true
 
-    abstract readonly quantifier: Quantifier
+    protected constructor(parent: SchemeNode | null, nodeDesc: NodeParams<TParams>) {
+        super(parent, nodeDesc)
+    }
 
-    /** Match node against the current token.  */
-    abstract Match(token: Token): MatchResult
+    /** Match node against the current token.
+     * @return True if token matched.
+     */
+    abstract Match(token: TToken): boolean
+
+    /** Evaluate the node to provide value for corresponding AST node. */
+    Evaluate(token: TToken): any {}
 
     /** Apply any custom transform for the token value. Default implementation just returns
      * unchanged value.
      */
-    TransformValue(value: TokenValue): any {
-        return value
+    //XXX
+    // TransformValue(value: TokenValue): any {
+    //     return value
+    // }
+}
+
+/** Represents next match candidate. */
+export abstract class NodeMatchCandidate {
+    abstract node: SchemeNode
+
+    /** Advance match position to next sibling node(s).
+     *
+     * @param forkIterator True to fork current iterator state, advance new instance and return it.
+     *  Otherwise mutate current iterator. Only one candidate can mutate current iterator. Fork can
+     *  happen only before the iterator state is mutated. Violating those conditions causes state
+     *  validation exception.
+     * @return Iterator for iterating candidates on the next position. Null if reached end of the
+     *  sequence.
+     */
+    abstract Next(forkIterator: boolean): NodeIterator | null
+
+
+    private readonly sequence: number
+
+    protected constructor(protected readonly iterator: NodeIterator)
+    {
+        this.sequence = iterator.sequence
+    }
+
+    /** Should be called by {@link Next} method implementation. */
+    protected ValidateNext() {
+        if (this.sequence != this.iterator.sequence) {
+            throw new Error("Attempting to advance already mutated iterator")
+        }
     }
 }
 
-abstract class SchemeInterimNode implements SchemeNode {
-    readonly isLeaf: boolean = false
+/** Iterates next match candidates. */
+export abstract class NodeIterator {
+    /** Get child nodes candidates list to match current token against. */
+    abstract GetCandidates(): Generator<NodeMatchCandidate>
 
-    abstract readonly quantifier: Quantifier
-
-    /** Get next child nodes to match current token against.
-     *
-     * @param ctx - Context if node is instantiated, `undefined` if the first match attempt.
-     */
-    abstract GetNextChild(ctx: SchemeNodeContext | null): Generator<SchemeNode>
-
-    /**
-     * Report matched node.
-     * @param ctx - Current context if not first match. It may be modified if `forkCtx` is false.
-     *  Otherwise new context should be returned.
-     * @param node - Matched node. One of nodes previously returned by `GetNextChild()`.
-     * @param forkCtx True if new context should be created and the passed one left untouched. False
-     *  if passed context may be modified.
-     * @returns Context with the match applied. May be passed context if `forkCtx` is false.
-     */
-    abstract ReportMatch(ctx: SchemeNodeContext | null, node: SchemeNode, forkCtx: boolean):
-        SchemeNodeContext
+    /** Should be incremented with each state mutation. */
+    sequence: number = 0
 }
+
+export abstract class SchemeInterimNode<TParams> extends SchemeNode {
+    readonly isTerminal: boolean = false
+
+    protected constructor(parent: SchemeNode | null, nodeDesc: NodeParams<TParams>) {
+        super(parent, nodeDesc)
+    }
+
+    /** Get iterator for child nodes iteration. */
+    abstract GetIterator(): NodeIterator
+
+    /** Evaluate the node to provide value for corresponding AST node. */
+    //XXX child nodes evaluation results, discard/replace option
+    Evaluate(): any {}
+}
+
+/**
+ * Build scheme from scheme description object.
+ * @param rootNodeDesc - Descriptor for scheme root node.
+ * @return Scheme root node.
+ */
+export function BuildScheme(rootNodeDesc: NodeDesc, parent: SchemeNode | null = null): SchemeNode {
+    return rootNodeDesc.factory(parent, rootNodeDesc)
+}
+
+export type OrderedGroupParams = {
+    content: NodeDesc[]
+}
+
+export const OrderedGroup = MakeNodeFactory(class extends SchemeInterimNode<OrderedGroupParams> {
+    private content: SchemeNode[]
+
+    constructor(parent: SchemeNode | null, nodeDesc: NodeParams<OrderedGroupParams>) {
+        super(parent, nodeDesc)
+        this.content = nodeDesc.content.map(desc => BuildScheme(desc, this))
+    }
+
+    override GetIterator(): NodeIterator {
+        throw new Error("Method not implemented.")
+    }
+})
+
+//XXX helpers for typical iterators (e.g. single item)
+
+export type NodeRefParams = {
+    refId: any
+}
+
+export const NodeRef = MakeNodeFactory(class NodeRef extends SchemeInterimNode<NodeRefParams> {
+    private ref: SchemeNode
+
+    constructor(parent: SchemeNode | null, nodeDesc: NodeParams<NodeRefParams>) {
+        super(parent, nodeDesc)
+        this.ref = this.GetGrammar().LookupSymbol(nodeDesc.refId)
+    }
+
+    override GetIterator(): NodeIterator {
+        //XXX
+        throw new Error("Method not implemented.")
+    }
+})
